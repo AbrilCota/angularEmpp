bd4fb635124ac8d3ad7641c4739b59165c32a31b
AbrilCota <62899668+AbrilCota@users.noreply.github.com>
2023-01-12 21:53:56 -0700

    SortingAndFilterCommit

--- src/app/employee/show-emp/show-emp.component.ts
+++ src/app/employee/show-emp/show-emp.component.ts
@@ -141,4 +141,13 @@ export class ShowEmpComponent implements OnInit{
     });
   }
 
+  sortResult(prop,asc){
+    this.EmployeeList = this.EmployeeListWithoutFilter.sort(function(a,b){
+      if(asc){
+        return (a[prop]>b[prop])?1 : ((a[prop]<b[prop]) ?-1 :0);
+      }else{
+        return (b[prop]>a[prop])?1 : ((b[prop]<a[prop]) ?-1 :0);
+      }
+    })
+  }
 }
